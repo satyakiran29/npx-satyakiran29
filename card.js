b65a79e6e2137f8715944d5c0a43f1873105db24
Satyakiran <satyakiran293@gmail.com>
2022-02-24 22:47:09 +0530

    ss

---
+++ card.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+const config = require('./config');
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:" + config.Mail);
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green(config.yourName),
+    handle: chalk.white(config.twitterHandle),
+    work: `${chalk.white(config.workat)} ${chalk
+        .hex("#2b82b2")
+        .bold(config.workPlace)}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan(config.twitterUsername),
+    github: chalk.gray("https://github.com/") + chalk.green(config.githubUsername),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue(config.linkedinUsername),
+    web: chalk.cyan(config.websiteUrl),
+    npx: chalk.red("npx") + " " + chalk.white(config.npxName),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    yourName: "                        Satya Kiran",
+    Mail: "contact.psatyakiran@gmail.com",
+    twitterHandle: "@SatyaKiran_29",
+    workat: "Computer Science & Engineering Student at",
+    workPlace: "Kiet",
+    twitterUsername: "Satyakiran",
+    githubUsername: "satyakiran29",
+    linkedinUsername: "Satyakiran",
+    websiteUrl: "https://satyakiran.ml",
+    npxName: "satyakiran29",
+};
