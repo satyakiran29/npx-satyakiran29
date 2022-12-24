9ae72e7200d09046daba7e65931592f92860105d
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2022-12-24 12:15:31 +0530

    Website linked update

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     yourName: "                        Satya Kiran",
     Mail: "contact.psatyakiran@gmail.com",
-    twitterHandle: "@SatyaKiran_29",
+    twitterHandle: "@satyaKiran029",
     workat: " Diploma Computer Science & Engineering Student at",
     workPlace: "Kiet",
-    twitterUsername: "Satyakiran_29",
+    twitterUsername: "satyakiran029",
     instagraminUsername: "satyakiran29",
     githubUsername: "satyakiran29",
     linkedinUsername: "Satyakiran",
-    websiteUrl: "https://satyakiran.ml",
+    websiteUrl: "https://satyakiran.pages.dev/#/",
     npxName: "satyakiran29",
 };
